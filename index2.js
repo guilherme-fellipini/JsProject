207467af429b7bd4a34b71bcfa0715a603b70e09
Guilherme Martins <fellipini@gmail.com>
2022-12-08 15:33:35 -0300

    switch

---
+++ index2.js
@@ -0,0 +1,22 @@
+let cor = "rosa";
+
+switch (cor) {
+  case "verde":
+    console.log("siga");
+    break;
+
+  case "amarelo":
+    console.log("atencao");
+    break;
+
+  case "vermelho":
+    console.log("pare");
+    break;
+
+  default:
+    console.log("nao sei");
+}
+
+//case condição : (entao) instruções; break;
+// dafault: se nada der certo o padrão é
+// continue: em vez de parar o laço ele continua e ignora todas as instruções , vai pra proxima interação
