752998e3a2e7fadc26c100cf0dee870bab8accdb
Guilherme Martins <fellipini@gmail.com>
2022-12-08 15:49:58 -0300

    first contas

---
+++ index3.js
@@ -0,0 +1,11 @@
+//taboada do 5, for, let i se inicia do numero 0 , vai ter q ir até ser menor ou igual a 10 de 1 por 1 ++
+
+let n = 7;
+
+for (let i = 0; i <= 10; i++) {
+  console.log(`${i}  ${n} = ${i * n}`); // essa forma melhor
+  // console.log(i + " x " + n + " = " + i * n); essa forma padrao antiga..
+}
+
+//template string
+//`recurso do ${js}`
